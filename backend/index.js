1545e1851cab69901baa5541bd12560dfae5d2c2
Your Name <you@example.com>
2023-12-19 18:07:57 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
-console.log('hi');
+const express = require('express') 
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const PORT = 5000
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log("Server is Running");
+  });
+
+  app.get("/Test", (req, res) => {
+    res.send({
+      message: "Hai",
+    });
+  });
+  
--- client/src/Admin/Components/Home.jsx
+++ client/src/Admin/Components/Home.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/Test/').then((response) =>{
+      console.log(response);
+    })
+  },[])
   return (
     <div>Home</div>
   )
